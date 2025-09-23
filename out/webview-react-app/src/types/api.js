8026b936680050deb4095c4adcdb9acf9f72d4e8
damhs <ihansaem1@kaist.ac.kr>
2025-09-23 15:48:10 +0200

    refactor: remove unnecessary files

--- out/webview-react-app/src/types/api.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// API 관련 공통 타입 정의
-Object.defineProperty(exports, "__esModule", { value: true });
--- src/log/formatPrompt.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// src/log/formatPrompt.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatPrompt = formatPrompt;
-function formatPrompt(label, snippet) {
-    return `너는 GitHub Actions 로그 분석 도우미야. 아래는 실패한 로그 파일 "${label}"의 내용이야. 실패 원인을 추론해서 설명해줘.\n\n\`\`\`\n${snippet}\n\`\`\``;
-}
