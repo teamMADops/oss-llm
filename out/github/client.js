1d8a8bf1b37d6657e23706d9648828b2445ea843
Daeyoung Kim <eodudrepublic@gmail.com>
2025-10-08 14:03:52 +0900

    chore: Delete `out` directory
    - npm run build시 생성되는 `out` 브랜치 삭제

--- out/github/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeOctokit = void 0;
-const rest_1 = require("@octokit/rest");
-const makeOctokit = () => new rest_1.Octokit({ auth: process.env.GITHUB_TOKEN });
-exports.makeOctokit = makeOctokit;
